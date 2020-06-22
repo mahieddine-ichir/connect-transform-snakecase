5a8d405f4d8058f2c36e1756e32e1330c65899ac
Mahieddine ICHIR <mahieddine.ichir@gmail.com>
2020-06-22 11:00:53 +0200

    Adding a unit test on SnakeTransformer

---
+++ src/test/java/net/thinatech/connect/transform/SnakeTransformerTest.java
@@ -0,0 +1,41 @@
+package net.thinatech.connect.transform;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+
+public class SnakeTransformerTest {
+
+    @Test
+    public void convert() throws IOException {
+
+        byte[] jsonBytes = Files.readAllBytes(Paths.get(this.getClass().getResource("/data.json").getPath()));
+
+        byte[] transformed = new SnakeTransformer().convert(jsonBytes);
+
+        Map output = new ObjectMapper().readValue(transformed, Map.class);
+
+        Assert.assertEquals("ichir", output.get("id"));
+        Assert.assertEquals("mahieddine.ichir@gmail.com", output.get("user_email"));
+
+        Map civility = (Map) output.get("civility_information");
+        Assert.assertEquals("mahieddine", civility.get("first_name"));
+        Assert.assertEquals("ichir", civility.get("last_name"));
+
+        Map address = (Map) civility.get("address");
+        Assert.assertEquals("10 Rue du vélo", address.get("line1"));
+        Assert.assertEquals("FR", address.get("country_reference"));
+
+        List tags = (List) output.get("tags");
+        Map tag1 = (Map) tags.get(1);
+
+        Assert.assertEquals("tag2", tag1.get("name"));
+        Assert.assertEquals("tagCat2", tag1.get("tag_category"));
+    }
+}
